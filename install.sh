604a07dacbb52099a00cfb2d2541d6bd4b499d7e
Emil Kjelsrud <emil.kjelsrud@intility.no>
2022-03-11 11:09:12 +0100

    fix install script formatting

--- install.sh
+++ install.sh
@@ -1,21 +1,28 @@
 #!/usr/bin/bash
 
+GREEN=$(tput setaf 2)
+BLUE=$(tput setaf 4)
+NORMAL=$(tput sgr0)
+BRIGHT=$(tput bold)
+COLOR=$BLUE
+STAR='\U1F31F'
+
 kind create cluster --name my-cluster --config ./cluster-config.yaml
 
-printf "\ninstalling nginx ingress controller..."
+printf "\n${STAR}${COLOR}installing nginx ingress controller...${NORMAL}\n"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
-printf "\nwaiting for ingress controller to get ready..."
+printf "\n${STAR}${COLOR}waiting for ingress controller to get ready...${NORMAL}\n"
 kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=90s
 
-printf "\ninstalling argocd..."
+printf "\n${STAR}${COLOR}installing argocd...${NORMAL}\n"
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-printf "\nconfiguring argocd..."
+printf "\n${STAR}${COLOR}configuring argocd...${NORMAL}\n"
 kubectl apply -f argocd-cmd-params-cm.yaml
 kubectl rollout restart deployment argocd-server -n argocd
 kubectl wait -n argocd \
@@ -23,11 +30,16 @@ kubectl wait -n argocd \
   --timeout=90s \
   --selector=app.kubernetes.io/name=argocd-server
 
-printf "\nexposing web UI..."
+printf "\n${STAR}${COLOR}exposing web UI...${NORMAL}\n"
 kubectl apply -f argocd-ingress.yaml
 
 argo_password=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
-printf "\nArgoCD now available at http://argocd.local"
-printf "admin:$argo_password"
+argo_host=$(kubectl -n argocd get ingress argocd-server-ingress -o jsonpath="{.spec.rules[0].host}")
+argo_path=$(kubectl get ingress -n argocd argocd-server-ingress -o jsonpath="{.spec.rules[0].http.paths[0].path}")
+printf "\nArgoCD now available at http://${argo_host:=localhost}$argo_path"
+printf "\nusername: ${BRIGHT}admin${NORMAL}"
+printf "\npassword: ${BRIGHT}$argo_password${NORMAL}"
 
-printf "\ndone."
+printf "\n\nNOTE:"
+printf "\nbootstrap argocd with:"
+printf "\n > kubectl apply -f argocd-bootstrapper.yaml"
